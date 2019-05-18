39e13d0b022118bf9540cbc2383cc905525533d9
Hector Roberto <hectorroberto.ti@gmail.com>
2019-05-18 18:39:58 -0500

    Pag Perfil
    nolasgod: Criação da página de perfil.
    Co-Authored-By: nolasgod <nolasgod@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule' }
+
 ];
 
 @NgModule({
